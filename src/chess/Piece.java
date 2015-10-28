f0fb506da91d89e030225d39133bf835eb0058a7
unknown <444003359@mvhsrop901-03.vusdnoc.vusd.k12.ca.us>
2015-10-28 14:24:14 -0700

    Initial Commit

---
+++ src/chess/Piece.java
@@ -0,0 +1,28 @@
+package chess;
+import java.awt.*;
+
+public class Piece {
+    private Color color;
+    private int value;
+    Piece(Color _color)
+    {
+        color = _color;
+        value = (int)(Math.random() * 4+1);
+    }
+    Color getColor()
+    {
+        return (color);
+    }
+    void setColor(Color _color)
+    {
+        color = _color;
+    } 
+    int getValue()
+    {
+        return(value);
+    }
+    void setValue(int _value)
+    {
+        value = _value;
+    }
+}
